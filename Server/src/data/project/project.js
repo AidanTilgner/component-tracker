ee635306e08f6b4b1c28b050cd255b59f71d2ced
Aidan Tilgner <aidantilgner02@gmail.com>
2022-01-05 11:15:42 -0800

    added some files for data control on the backend
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

---
+++ Server/src/data/project/project.js
@@ -0,0 +1,23 @@
+class Project {
+  constructor(
+    id,
+    owner,
+    contributors,
+    name,
+    description,
+    framework,
+    languages,
+    externalLinks,
+    components
+  ) {
+    this.id = id;
+    this.owner = owner;
+    this.contributors = contributors;
+    this.name = name;
+    this.description = description;
+    this.framework = framework;
+    this.languages = languages;
+    this.externalLinks = externalLinks;
+    this.components = components;
+  }
+}
